b4d18557c5956d7822b74d2a1fb5f26aeb54bfe4
Radiatechs-Mayank <103631131+Radiatechs-Mayank@users.noreply.github.com>
2022-07-05 14:39:49 +0530

    saving-without-encryption

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const io = new Server(server);
 var disconnectName;
 var online = 0;
 
+// sqlite related code
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./mydb.db3');
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/name.html');
 });
@@ -31,8 +35,20 @@ io.on('connection', (socket) => {
 
   // chat message functionality
   socket.on('chat message', (data) => {
+    db.serialize(() => {
+      db.run("CREATE TABLE IF NOT EXISTS chats (sender TEXT, message TEXT)");                   // Runs the SQL query with the specified parameters and calls the callback afterwards. It does not retrieve any result data. The function returns the Database object for which it was called to allow for function chaining.
+
+      const stmt = db.prepare("INSERT INTO chats VALUES (?,?)");    // Prepares the SQL statement and optionally binds the specified parameters and calls the callback when done.
+      stmt.run(data.username, data.usermessage);                                 //Binds parameters and executes the statement. The function returns the Statement object to allow for function chaining.
+      stmt.finalize();                                            // This is typically optional, but if you experience long delays before the next query is executed, explicitly finalizing your statement might be necessary
+  
+      db.each("SELECT * FROM chats", (err, row) => {  // Runs the SQL query with the specified parameters and calls the callback once for each result row. 
+          console.log(row.sender + ": " + row.message);
+      });
+  });
+  
+  // db.close();                                                         // Closes the database.
     io.emit('chat message', data);
-    console.log('mesage: ', data);
   });
 
   // some user disconnected functionality
